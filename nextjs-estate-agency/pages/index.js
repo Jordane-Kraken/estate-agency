640ae52809049f320ca78bc8b5063c59707f54c9
Jordane <jordane@dignamik.fr>
2021-07-20 10:37:56 +0200

    yarn create next app done

---
+++ nextjs-estate-agency/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Layout } from "../components/layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      bonjour
+    </Layout>
+  )
+}
+
+export default Home;
